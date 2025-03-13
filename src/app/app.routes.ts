2f89963bb64c075b1bb970c3a71a87b214d46a8c
DevDeivy <deivylujan@hotmail.com>
2025-03-13 00:15:10 -0500

    :boom: feature/Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { rout } from './auth/rout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    ...rout
+];
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from "./login/login.component";
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  imports: [],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css'
+})
+export class AuthComponent {
+
+}
