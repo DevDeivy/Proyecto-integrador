0f0277dac40ea899200148dc0a398b3e81b7d142
Daniel Andres Cabana Trejos <danielcabana0727@gmail.com>
2025-03-29 23:59:50 -0500

    :hammer: [ADD] First version of authentication API is ready.

---
+++ backend/authentication/src/main/java/culti/authentication/infrastructure/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package culti.authentication.infrastructure.repository;
+
+import culti.authentication.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail (String email);
+}
