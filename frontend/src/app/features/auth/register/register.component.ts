7f0e725429f502afbf2de606f8427eae7dd15ecf
Daniel Andres Cabana Trejos <danielcabana0727@gmail.com>
2025-04-08 01:23:50 -0500

    :hammer: [integrate] Connected frontend with backend API, enabled token handling and configured CORS permissions.

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
+import { AuthenticationService } from '../../../core/authentication.service';
 
 @Component({
   selector: 'app-register',
   imports: [
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  authService: AuthenticationService = inject(AuthenticationService);
+
+  register = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+
+  registerUser() {
+
+    const { email, password, confirmPassword } = this.register.value;
+
+    if (password === confirmPassword) {
+
+      const userData = {
+        email,
+        password,
+      }
+
+      this.authService.registerUser(userData).subscribe({
+
+        next: res => {
+          console.log(res);
+        },
+        error: e => {
+          console.log(e);
+        }
+
+      });
+
+    };
+
+  };
 
 }
