2ea4862457e71f08ef960c4c221ed7eca32e0c8d
Daniel Andres Cabana Trejos <danielcabana0727@gmail.com>
2025-05-01 15:53:03 -0500

    :construction: [ADD] component was added to display products.

--- frontend/src/app/features/rout.ts
+++ frontend/src/app/features/rout.ts
@@ -2,6 +2,11 @@ import { Routes } from "@angular/router";
 import { LoginComponent } from "./auth/login/login.component";
 import { RegisterComponent } from "./auth/register/register.component";
 import { HomeComponent } from "./home/home.component";
+import { LetterProductComponent } from "./letter-product/letter-product.component";
+import { ListProductsComponent } from "./list-products/list-products.component";
+import { FgpasswordComponent } from "./auth/fgpassword/fgpassword.component";
+import { FgcodepasswordComponent } from "./auth/fgcodepassword/fgcodepassword.component";
+import { NewpasswordComponent } from "./auth/newpassword/newpassword.component";
 
 
 export const rout: Routes = [
@@ -9,6 +14,14 @@ export const rout: Routes = [
         path: "",
         redirectTo: "home",
         pathMatch: "full"
+    },
+    {
+        path: "list-products",
+        component: ListProductsComponent
+    },
+    {
+        path: "letter-product",
+        component: LetterProductComponent
     },
     {
         path: "home",
@@ -22,4 +35,16 @@ export const rout: Routes = [
         path: "register",
         component: RegisterComponent
     },
+    {
+        path: "fgtpass",
+        component: FgpasswordComponent
+    },
+    {
+        path: "fgtcode",
+        component: FgcodepasswordComponent
+    },
+    {
+        path: "newpass",
+        component: NewpasswordComponent
+    }
 ];
