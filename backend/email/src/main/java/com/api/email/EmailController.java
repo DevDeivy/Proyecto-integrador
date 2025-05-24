9e0446cc7072f4c683a26f678b7fe07418015c56
Daniel Andres Cabana Trejos <danielcabana0727@gmail.com>
2025-05-23 19:17:36 -0500

    :sparkles: [ADD] Added email feature.

---
+++ backend/email/src/main/java/com/api/email/EmailController.java
@@ -0,0 +1,22 @@
+package com.api.email;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/send/email")
+public class EmailController {
+    private SendMessageService sendMessageService;
+
+    public EmailController(SendMessageService sendMessageService) {
+        this.sendMessageService = sendMessageService;
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<Object> sendEmail(@RequestBody EmailDTO emailDTO){
+        return  sendMessageService.sendSimpleMessage(emailDTO);
+    }
+}
